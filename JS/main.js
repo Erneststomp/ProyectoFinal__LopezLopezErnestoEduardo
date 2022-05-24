508c82d49486d33acf181c8003198dd5a46e5113
Erneststomp <ernesto.lopez.lbm@outlook.com>
2022-05-24 18:51:26 -0500

    Actializacion, el programa ya permite modificar los datos de los empleados aunque aun no es capaz de limpiar los erroneos de la pantalla

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,16 @@ let password__1;
 var Empleados__1;
 var modificador=1;
 var save_changes=0;
-var change;
+var change=0;
+let matrix_emplee=[];
+let matrix_emplee_name=[];
+let matrix_emplee_lastname=[];
+let matrix_emplee_age=[];
+let matrix_emplee_sex=[];
+let matrix_emplee_job=[];
+let matrix_emplee_study=[];
+
+
 //inicio del programa 
 //acceso mediante contraseña
 
@@ -49,6 +58,7 @@ function Empleador__registro(){
     console.log(Empleados);
     localStorage.setItem("Numerodeempleados",Empleados);
     window.location.href = "./register.html";
+
 }
 
 
@@ -56,6 +66,7 @@ function Empleador__registro(){
 function agregados (){
             var output=document.querySelector(".output");
             console.log(Empleados);
+
             const data={
                 nombre: document.getElementById("NAME").value,
                 apellido: document.getElementById("LASTNAME").value,
@@ -65,9 +76,36 @@ function agregados (){
                 estudios: document.getElementById("STUDIES").value,
                 numero: i+1
             };
-            console.log(data);
-            localStorage.setItem('Datos_de_Empleados', JSON.stringify(data));
 
+            localStorage.setItem('Datos_de_Empleados', JSON.stringify(data));
+            if(change == 0) {
+                console.log(data.nombre);
+                matrix_emplee_name[i]=data.nombre;
+                matrix_emplee_lastname[i]=data.apellido;
+                matrix_emplee_age[i]=data.edad;
+                matrix_emplee_sex[i]=data.sexo;
+                matrix_emplee_job[i]=data.puesto;
+                matrix_emplee_study[i]=data.estudios;
+            }
+            else{
+                matrix_emplee_name[modificador]=data.nombre;
+                matrix_emplee_lastname[modificador]=data.apellido;
+                matrix_emplee_age[modificador]=data.edad;
+                matrix_emplee_sex[modificador]=data.sexo;
+                matrix_emplee_job[modificador]=data.puesto;
+                matrix_emplee_study[modificador]=data.estudios;
+                data.numero=modificador+1;
+            }
+
+
+            localStorage.setItem('matriz_empleados_nombre', JSON.stringify(matrix_emplee_name));
+            localStorage.setItem('matriz_empleados_apellido', JSON.stringify(matrix_emplee_lastname));
+            localStorage.setItem('matriz_empleados_edad', JSON.stringify(matrix_emplee_age));
+            localStorage.setItem('matriz_empleados_sexo', JSON.stringify(matrix_emplee_sex));
+            localStorage.setItem('matriz_empleados_trabajo', JSON.stringify(matrix_emplee_job));
+            localStorage.setItem('matriz_empleados_estudios', JSON.stringify(matrix_emplee_study));
+
+            console.log(i)
             //imprime los datos en pantalla
                 let newlist=document.createElement('div');
                 newlist.classList.add('Empleados__CSS');
@@ -94,7 +132,7 @@ function agregar (){
     var storedValue = localStorage.getItem("Numerodeempleados");
     Empleados=storedValue;
     console.log(Empleados)
-        if (i<Empleados){
+        if (i<Empleados||change==1){
                 agregados()
         }
         else{
@@ -109,8 +147,16 @@ function Modificar__Datos(){
     console.log(data);
     data = JSON.parse(data);
     console.log(data);
-    modificador=prompt('¿Que numero de empleado buscas modificiar? (de momento solo se guarda un empleado en memoria por tanto solo se modifica el 1 o en caso de que guarde muchos, solo se modificara el ultimo)');
-    modificador=1;
-    change=prompt('Digite el dato que desea modificar: 1) Nombre 2) Apellido 3) Edad 4) Sexo 5)Educacion 6)Puesto');
+    modificador=prompt('¿Que numero de empleado desea modificiar?');
+    modificador=modificador-1;
+    localStorage.getItem('matriz_empleados_nombre');
+    localStorage.getItem('matriz_empleados_apellido');
+    localStorage.getItem('matriz_empleados_edad');
+    localStorage.getItem('matriz_empleados_sexo');
+    localStorage.getItem('matriz_empleados_trabajo');
+    localStorage.getItem('matriz_empleados_estudios');
+    change=1;
+
 }
+
 
